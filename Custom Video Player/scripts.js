7145c3f17384626f5acc455d1fa35042823674a8
rohitjaiswal-3878 <rj710910@gmail.com>
2024-06-26 13:11:11 +0530

    Custom video player

---
+++ Custom Video Player/scripts.js
@@ -0,0 +1,71 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+const fullScreen = player.querySelector(".full__screen");
+
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "▶" : "▐▐ ";
+  toggle.innerText = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log("handle change range.");
+}
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+}
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function toggleFullScreen(toggleFs) {
+  if (toggleFs) {
+    player.classList.add("myVideo");
+  } else {
+    player.classList.remove("myVideo");
+  }
+}
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((skipButton) => skipButton.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mouseDown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mouseDown && scrub(e));
+progress.addEventListener("mousedown", () => (mouseDown = true));
+progress.addEventListener("mousedown", () => (mouseDown = false));
+
+let toggleFs = false;
+fullScreen.addEventListener("click", () => {
+  toggleFullScreen(!toggleFs);
+  toggleFs = !toggleFs;
+});
